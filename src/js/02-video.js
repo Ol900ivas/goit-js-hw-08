89f7440b4a9c2305e05c4b52b47048994d28d42d
Olga Lorenz <ol900ivas@gmail.com>
2023-02-09 12:46:53 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const savedTime = localStorage.getItem("videoplayer-current-time");
+
+
+const parsedTime = JSON.parse(savedTime);
+
+savedTime ? player.setCurrentTime(parsedTime) : player.setCurrentTime(0);
+
+const onPlay = function (data) {
+    const userTime = data.seconds;
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(userTime));
+    console.log(userTime);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
 
